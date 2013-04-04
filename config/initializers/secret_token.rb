147675c68cfb24da4c75a4e58e01c41531c9b0db
Rick Thomas <rthomas@igodigital.com>
2013-04-04 15:33:29 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CanaryCanary::Application.config.secret_token = 'aec212b8c53f12f75e9306e12164e0f2ea02a1d250422738e5c936ebeac1e4653a8a26a013cf6b9c7ce5382b061969acab693694b136c883fd8c1a9345194bd2'
