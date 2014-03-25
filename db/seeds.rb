3dba9adcac29615d79989cc69072a827da527f88
Rick Thomas <rick@rtindy.com>
2014-03-25 16:43:29 -0400

    tweak seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,7 @@ require 'csv'
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.com/rails-environment-variables.html
-puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
+["admin", "user", "VIP"].each do |role|
   Role.find_or_create_by_name({ :name => role }, :without_protection => true)
   puts 'role: ' << role
 end
